e2a315b3d3d513b616658a6b367f5afa48fc694d
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-02-21 17:43:49 -0800

    new unity project

---
+++ CSE 4410 Pong/Assets/Scripts/PongPaddleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongPaddleController : MonoBehaviour
+{
+    public bool p1;
+    public float spd; 
+    Rigidbody2D bod;
+
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (p1)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                bod.AddForce(Vector2.up * spd * Time.deltaTime); 
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                bod.AddForce(-Vector2.up * spd * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                bod.AddForce(Vector2.up * spd * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                bod.AddForce(-Vector2.up * spd * Time.deltaTime);
+            }
+        }
+    }
+}
