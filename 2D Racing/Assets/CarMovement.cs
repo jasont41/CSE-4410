4bc33d60a6bd864dbca44c9d43726c19f0352f3a
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-03-07 00:27:32 -0800

    Car is moving!

---
+++ 2D Racing/Assets/CarMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float acceleration;
+    public float rotation;
+    public float maxSpeed;
+    public float currentSpeed; 
+    Rigidbody2D bod;
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>(); 
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            bod.AddForce(Vector2.up * acceleration * Time.deltaTime); 
+        }
+    }
+}
---
+++ 2D Racing/Assets/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string turning()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            return "left"; 
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            return "right";
+        }
+        else return " " ; 
+    }
+}
