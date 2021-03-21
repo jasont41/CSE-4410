c48929956cadba17b4f04d83d8444f30a45acb42
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-03-21 16:07:19 -0700

    TD progress

--- CSE 4410 Tower Defense Minigame/Assets/Scripts/TDEnemyController.cs
+++ CSE 4410 Tower Defense Minigame/Assets/Scripts/TDEnemyController.cs
@@ -4,9 +4,71 @@ using UnityEngine;
 
 public class TDEnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D bod;
+    public float spd;
+    public float maxHP;
+    float hp;
+    TDGameController cont; 
+
+    Transform target;
+    public float rotSpd;
+    int curWaypoint;
+    float distance;
+    public float dmg; 
+
+    bool canMove = true;
+    bool hasHurt = false; 
+    private void Awake()
+    {
+        bod = GetComponent<Rigidbody2D>(); 
+        cont = FindObjectOfType<TDGameController>();
+    }
+    private void OnEnable()
+    {
+        hp = maxHP;
+        curWaypoint = 0;
+        hasHurt = false; 
+        target = cont.waypoints[curWaypoint]; 
+    }
+    private void Update()
+    {
+        Vector3 dir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotSpd);
+
+        if(canMove) bod.AddForce(transform.up * spd * Time.deltaTime);
+
+        distance = Vector2.Distance(transform.position, target.position); 
+        if(distance <= 0.1f)
+        {
+            if(curWaypoint < cont.waypoints.Length - 1)
+            {
+                canMove = false;
+                Invoke("CanMove", 1f); 
+                curWaypoint++;
+                target = cont.waypoints[curWaypoint]; 
+            }
+            else
+            {
+                if (!hasHurt)
+                {
+                    cont.TakeDamage(dmg);
+                    hasHurt = true; 
+                }
+                gameObject.SetActive(false); 
+            }
+        }
+    }
+    void CanMove()
+    {
+        canMove = true; 
+    }
     public void TakeDamage(float amt)
     {
-
+        hp -= amt; 
+        if(hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
--- CSE 4410 Tower Defense Minigame/Assets/Scripts/TDGameController.cs
+++ CSE 4410 Tower Defense Minigame/Assets/Scripts/TDGameController.cs
@@ -1,13 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TDGameController : MonoBehaviour
 {
     public GameObject tower;
-    public float cost; 
-    public void SpawnTower()
+    public float cost;
+    public Transform[] waypoints;
+    public float maxHP;
+    public float hp;
+    public Image healthImage;
+    public float lerpSpd; 
+    private void Awake()
     {
-
+        hp = maxHP;
+        healthImage.fillAmount = Mathf.Lerp(healthImage.fillAmount, hp / maxHP, lerpSpd * Time.deltaTime);
+    }
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Time.timeScale *= 2f; 
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Time.timeScale *= 1f;
+            }
+        }
+    }
+    public void TakeDamage(float amt)
+    {
+        hp -= amt;
+        healthImage.fillAmount = Mathf.Lerp(healthImage.fillAmount, hp / maxHP, lerpSpd * Time.deltaTime);
     }
 }
