3c4011c791540fee230038f844148c77068b0f1a
jasont41 <43120995+jasont41@users.noreply.github.com>
2021-05-16 15:04:42 -0700

    Final commit

--- 2D Racing/Assets/CarMovement.cs
+++ 2D Racing/Assets/CarMovement.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class CarMovement : MonoBehaviour
 {
     public float acceleration;
@@ -14,17 +15,24 @@ public class CarMovement : MonoBehaviour
     public float speed;
     public bool freezeMovement;
     public bool timeStarted; 
-    public Text text; 
+    public Text text;
+    public bool halfwayCheck;
+    public float finalTime; 
     Rigidbody2D bod;
     private IEnumerator coroutine;
 
+    public Button start;
+    public Button Restart;
+    public Button MainMenu; 
+
     //timing variables
-    private float seconds = 0.0f;
-    private float minutes = 0.0f;
-    private float milliseconds = 0.0f;
     private float startTime; 
     private void Awake()
     {
+        Restart.gameObject.SetActive(false);
+        MainMenu.gameObject.SetActive(false); 
+        halfwayCheck = false;
+        text.text = " "; 
         bod = GetComponent<Rigidbody2D>();
         freezeMovement = false;
         timeStarted = false; 
@@ -41,12 +49,13 @@ public class CarMovement : MonoBehaviour
         }
         if (timeStarted) {
             startTime += Time.deltaTime;
-            text.text = startTime.ToString(); 
+            text.text = startTime.ToString("F2"); 
         }
 
     }
     public void setBoolTrue()
     {
+        start.gameObject.SetActive(false); 
         coroutine = countDown();
         StartCoroutine(coroutine);
         freezeMovement = true;
@@ -66,5 +75,23 @@ public class CarMovement : MonoBehaviour
         text.text = "0:00:00";
         timeStarted = true;
         startTime = 0.0f; 
+    }
+    public void finishRaceProtocal()
+    {
+        setBoolFalse();
+        finalTime = startTime;
+        text.text = finalTime.ToString("2F");
+        timeStarted = false;
+        Restart.gameObject.SetActive(true);
+        MainMenu.gameObject.SetActive(true);
+    }
+
+    public void restartButton()
+    {
+        SceneManager.LoadScene("SampleScene"); 
+    }
+    public void mainMenuButton()
+    {
+        SceneManager.LoadScene("Title Screen"); 
     }
 }
---
+++ 2D Racing/Assets/FinishCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishCollider : MonoBehaviour
+{
+    public CarMovement car;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && car.halfwayCheck == true)
+        {
+            car.finishRaceProtocal(); 
+        }
+    }
+}
